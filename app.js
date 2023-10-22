5d8cdcfaa0df2df76b785adf1882349c5cc8cb01
Евгений Петров <evgeniy161102@gmail.com>
2023-10-22 12:50:10 +0300

    Добавил регистрацию, POST

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express, { json } from "express";
 import jwt from "jsonwebtoken";
-import database from "./database.js";
+import bcrypt from "bcrypt";
+import { createNewUser, getUsers } from "./database.js";
 import { registerValidation } from "./validations/auth.js";
 const app = express();
 app.use(express.json());
@@ -9,11 +10,14 @@ import { validationResult } from "express-validator";
 
 const PORT = 3000;
 
+// Запрос на всез пользователей в БД
 app.get("/", async (req, res) => {
-  const users = await database.getUsers();
+  const users = await getUsers();
+
   if (users.length < 1) {
     return res.status(400).json({
       success: false,
+      message: "Пользователи не найдены",
     });
   } else {
     return res.json({
@@ -23,14 +27,50 @@ app.get("/", async (req, res) => {
   }
 });
 
-app.post("/auth/register", registerValidation, (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json(errors.array());
+// Создать пользователя
+app.post("/register", registerValidation, async (req, res) => {
+  try {
+    const body = req.body;
+    const password = body.password;
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
+    const errors = validationResult(req);
+
+    const user = await createNewUser(
+      body.fullName,
+      body.email,
+      passwordHash,
+      body.avatarUrl
+    );
+
+    const token = jwt.sign(
+      {
+        id: user,
+        ...body,
+      },
+      "secret123",
+      {
+        expiresIn: "30d",
+      }
+    );
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    } else {
+      return res.status(200).json({
+        success: true,
+        data: {
+          token,
+        },
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Не удалось совершить регистрацию",
+    });
+    throw error;
   }
-  res.json({
-    success: true,
-  });
 });
 
 app.listen(PORT, (err) => {
--- database.js
+++ database.js
@@ -3,13 +3,21 @@ import mysql from "mysql2";
 const pool = mysql.createPool({
   host: "127.0.0.1",
   user: "root",
-  database: "users",
+  database: "Users_bd",
   password: "root",
 });
 
 const promisePool = pool.promise();
 
-module.exports.getUsers = async function () {
+export const getUsers = async () => {
   let [rows] = await promisePool.query("SELECT * FROM `users`");
   return rows;
+};
+
+export const createNewUser = async (fullName, email, password, avatarUrl) => {
+  let [rows] = await promisePool.query(
+    "INSERT INTO `users`(`fullName`, `email`, `password`, `avatarUrl`) VALUES (?, ?, ?, ?)",
+    [fullName, email, password, avatarUrl]
+  );
+  return rows.insertId;
 };
